fcbcc9125b2037d68ab1815b9ae7fb330d9e3cc4
EunHyeYun <lillyeh@hatiolab.com>
2020-04-29 09:16:11 +0900

    동원 itnegrator 수정

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-import "url-polyfill";
-import "url-search-params-polyfill";
-import { SceneIntegrator } from "@things-scene/things-board-integration";
+import 'url-polyfill'
+import 'url-search-params-polyfill'
+import { SceneIntegrator } from '@things-scene/things-board-integration'
 
-export function init({
-  baseURL = "http://board-demo.hatiolab.com/rest",
-  queryString,
-  authorizationKey,
-  license = ""
-}) {
-  var fit = "both"; // 'both', 'ratio', 'center', 'none'
+export function init({ baseURL = 'https://deadpool.hatiolab.com/rest', queryString, authorizationKey, license = '' }) {
+  var fit = 'both' // 'both', 'ratio', 'center', 'none'
 
-  var searchParams;
+  var searchParams
 
   if (!queryString) {
-    return alert("Parameter가 존재하지 않습니다.");
+    return alert('Parameter가 존재하지 않습니다.')
   }
 
-  searchParams = new URLSearchParams(queryString);
+  searchParams = new URLSearchParams(queryString)
 
-  var sceneName = searchParams.get("scene");
-  var keepingTime =
-    new URL(window.location.href).searchParams.get("interval") || 30;
-  var list = sceneName.split(/\s*,\s*/);
-  var viewers = [];
-  var token = searchParams.get("token");
+  var sceneName = searchParams.get('scene')
+  var keepingTime = new URL(window.location.href).searchParams.get('interval') || 30
+  var list = sceneName.split(/\s*,\s*/)
+  var viewers = []
+  var token = searchParams.get('token')
 
   authorizationKey = token || authorizationKey;
 
   if (!sceneName) {
-    return alert("Scene name이 필요합니다.");
+    return alert('Scene name이 필요합니다.')
   }
 
   var integrator = SceneIntegrator.instance({
@@ -37,63 +31,63 @@ export function init({
     baseURL,
     fit,
     license
-  });
-  var index = 0;
-  var num = 0;
-  var boards = [];
+  })
+  var index = 0
+  var num = 0
+  var boards = []
 
   function showBoard(index) {
-    var targetId = "scene-viewer" + index;
-    let div = document.createElement("div");
-    div.setAttribute("id", targetId);
-    div.setAttribute("class", "scene-viewer");
-    div.hidden = true;
-    document.body.appendChild(div);
+    var targetId = 'scene-viewer' + index
+    let div = document.createElement('div')
+    div.setAttribute('id', targetId)
+    div.setAttribute('class', 'scene-viewer')
+    div.hidden = true
+    document.body.appendChild(div)
 
     integrator.integrate({
       target: targetId,
-      sceneName: "SCENE-" + list[index],
+      sceneName: 'SCENE-' + list[index],
       callback: callback1
-    });
+    })
   }
-  showBoard(index);
+  showBoard(index)
 
   // 인티그레이터 화면(div) 리스트 생성
   function playIntegrator(length, num, viewers) {
     // 자신의 순서인 viewer만 보이고 나머지는 전부 히든
     viewers.forEach((v, index) => {
-      v.hidden = num != index;
-    });
+      v.hidden = num != index
+    })
 
-    num = num + 1 < length ? num + 1 : 0;
+    num = num + 1 < length ? num + 1 : 0
 
-    boards.forEach(b => integrator.fit(fit, b));
+    boards.forEach(b => integrator.fit(fit, b))
 
-    setTimeout(function() {
-      playIntegrator(length, num, viewers);
-    }, keepingTime * 1000);
+    setTimeout(function () {
+      playIntegrator(length, num, viewers)
+    }, keepingTime * 1000)
   }
   // 콜백은 나중에 필요하면 사용
   function callback1(board, e) {
     if (e) {
-      console.error(e);
-      return;
+      console.error(e)
+      return
     }
 
-    boards.push(board);
+    boards.push(board)
 
-    window.addEventListener("resize", function() {
-      integrator.fit(fit, board);
-    });
+    window.addEventListener('resize', function () {
+      integrator.fit(fit, board)
+    })
 
     if (index == list.length - 1) {
-      viewers = Array.from(document.querySelectorAll(".scene-viewer"));
-      playIntegrator(list.length, num, viewers);
-      document.body.removeChild(document.getElementById("splash"));
+      viewers = Array.from(document.querySelectorAll('.scene-viewer'))
+      playIntegrator(list.length, num, viewers)
+      document.body.removeChild(document.getElementById('splash'))
     }
-    index++;
+    index++
     if (index < list.length) {
-      showBoard(index);
+      showBoard(index)
     }
   }
 }
